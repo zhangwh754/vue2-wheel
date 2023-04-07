c9d35b8b81aa431bc754fcaefc85dc2a30a8b3c6
张文皓 <zhangwh754@163.com>
2023-04-07 14:30:53 +0800

    ✨ feat: icon的基本样式完成

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,33 @@
   <div id="app">
     <Button />
     <Button label="你好" />
+    <hr />
+
+    <div class="bar">
+      <Icon icon="setting" />
+    </div>
   </div>
 </template>
 
 <script>
 import Button from './components/Button'
+import Icon from './components/Icon'
 
 export default {
   name: 'App',
   components: {
     Button,
-  },
+    Icon
+  }
 }
 </script>
 
 <style lang="scss">
 #app {
   margin: 20px;
+
+  .bar {
+    font-size: 40px;
+  }
 }
 </style>
---
+++ src/components/Icon/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <svg class="g-icon">
+    <use :xlink:href="`#i-${icon}`"></use>
+  </svg>
+</template>
+<script>
+export default {
+  props: {
+    icon: String
+  }
+}
+</script>
+<style lang="scss">
+.g-icon {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+}
+</style>
